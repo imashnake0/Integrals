c6ab37b911ff2be3f6dfb0b071fdaea425df66d1
imashnake0 <kamaleshpaluru07@gmail.com>
2022-07-11 17:23:41 -0400

    Added integral for the example
    With specific values.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,32 @@
 @file:Suppress("LocalVariableName")
 
+import core.Integral
+import core.Methods.*
+import kotlin.math.*
+
 fun main() {
-    
+    val R = 1.0
+    val r = sqrt(5.0)
+    val thetaP = 1.107148
+    val phiP = PI/4
+
+    val A_x = Integral { phi ->
+        -1*sin(phi)/sqrt(R*R + r*r - 2*R*r*sin(thetaP)*cos(phiP - phi))
+    }.integrate(
+        from = 0.0,
+        to = 2* PI,
+        differential = 0.0001,
+        method = TRAPEZOIDAL
+    )
+
+    val A_y = Integral { phi ->
+        cos(phi)/sqrt(R*R + r*r - 2*R*r*sin(thetaP)*cos(phiP - phi))
+    }.integrate(
+        from = 0.0,
+        to = 2* PI,
+        differential = 0.0001,
+        method = TRAPEZOIDAL
+    )
+
+    println("$A_x, $A_y")
 }
