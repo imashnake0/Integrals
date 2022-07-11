60b6c67b746bebccaf71ab707059cc77986c3753
imashnake0 <kamaleshpaluru07@gmail.com>
2022-07-11 17:52:21 -0400

    Printed magnitudes and components
    For the same specific circle as before.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,31 +2,36 @@
 
 import core.Integral
 import core.Methods.*
+import extensions.roundTo
 import kotlin.math.*
 
 fun main() {
     val R = 1.0
     val r = sqrt(5.0)
     val thetaP = 1.107148
-    val phiP = PI/4
+    var phiP = 0.0
 
-    val A_x = Integral { phi ->
-        -1*sin(phi)/sqrt(R*R + r*r - 2*R*r*sin(thetaP)*cos(phiP - phi))
-    }.integrate(
-        from = 0.0,
-        to = 2* PI,
-        differential = 0.0001,
-        method = TRAPEZOIDAL
-    )
+    repeat(100) {
+        val A_x = Integral { phi ->
+            -1*sin(phi)/sqrt(R*R + r*r - 2*R*r*sin(thetaP)*cos(phiP - phi))
+        }.integrate(
+            from = 0.0,
+            to = 2* PI,
+            differential = 0.00001,
+            method = TRAPEZOIDAL
+        )
 
-    val A_y = Integral { phi ->
-        cos(phi)/sqrt(R*R + r*r - 2*R*r*sin(thetaP)*cos(phiP - phi))
-    }.integrate(
-        from = 0.0,
-        to = 2* PI,
-        differential = 0.0001,
-        method = TRAPEZOIDAL
-    )
+        val A_y = Integral { phi ->
+            cos(phi)/sqrt(R*R + r*r - 2*R*r*sin(thetaP)*cos(phiP - phi))
+        }.integrate(
+            from = 0.0,
+            to = 2* PI,
+            differential = 0.00001,
+            method = TRAPEZOIDAL
+        )
 
-    println("$A_x, $A_y")
+        phiP += 2*PI/100
+
+        println("${(A_x*A_x + A_y*A_y).roundTo(3)}, ${A_x.roundTo(3)}, ${A_y.roundTo(3)}")
+    }
 }
